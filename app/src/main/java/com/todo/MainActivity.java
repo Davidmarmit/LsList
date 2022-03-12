26bb54b0a1c03ed55b607862eb23035b23b510fd
ivan <ohprope@gmail.com>
2022-03-12 13:18:30 +0100

    creacion de la classe adapter e incorporacion del recycler view en el xml y en la logica

--- app/src/main/java/com/todo/MainActivity.java
+++ app/src/main/java/com/todo/MainActivity.java
@@ -1,14 +1,51 @@
 package com.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ArrayList tareas= new ArrayList<>(Arrays.asList(R.id.tareas)); //hay que hacer el array de las tareas
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Getting reference of recyclerView
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        // Setting the layout as linear
+        // layout for vertical orientation
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        // Sending reference and data to Adapter
+        Adapter adapter = new Adapter(MainActivity.this, tareas);
+
+        // Setting Adapter to RecyclerView
+        recyclerView.setAdapter((RecyclerView.Adapter) adapter);
+    }
+/*
+        public void onClick(View view) {
+            openSecondActivity();
+        }*/
+
+
+    }
+    public void openSecondActivity(){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
